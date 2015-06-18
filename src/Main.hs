e38ad8dfba7aff4256330d5d8f9fea79408d0b39
Kirill Borisov <lensvol@gmail.com>
2015-06-18 22:56:14 +0300

    Small refactoring.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,3 @@
-import Data.List
-
 import System.Environment
 import System.IO
 import System.Random
@@ -9,19 +7,20 @@ type Fortune = [String]
 parseFortuneFile :: String -> [Fortune]
 parseFortuneFile contents = splitOn "%" (lines contents)
 
-splitOn :: (Eq a) => a -> [a] -> [[a]]
-splitOn delim [] = []
-splitOn delim mainList = splitOn' delim mainList [] []
+splitOn x ys =
+    let (y, ys') = break (== x) ys
+    in  if null ys'
+        then [y]
+        else y : splitOn x (tail ys')
 
-splitOn' :: (Eq a) =>  a -> [a] -> [a] -> [[a]] -> [[a]]
-splitOn' delim (x:xs) acc result
-  | null xs = result ++ [acc ++ [x]]
-  | x == delim = splitOn' delim xs [] (result ++ [acc])
-  | otherwise = splitOn' delim xs (acc ++ [x]) result
+getRandomFortune :: [Fortune] -> IO String
+getRandomFortune fortunes = do
+  randomNum <- getStdRandom (randomR (0, length fortunes))
+  return $ unlines (fortunes !! randomNum)
 
-main = do -- print $ splitOn "%" ["1", "2", "3", "%", "4", "5", "6"]
+main = do
   (datFileName:_) <- getArgs
   contents <- readFile datFileName
   let fortunes = parseFortuneFile contents
-  randomNum <- getStdRandom (randomR (0, length fortunes))
-  putStrLn $ unlines  (fortunes !! randomNum)
+  randomOne <- getRandomFortune fortunes
+  putStrLn $ randomOne
